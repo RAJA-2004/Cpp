d5e223395998af08f4e98e17b07d7ccb4deea2e8
RAJA DIGVIJAY SINGH <rajadigvijay2004@gmail.com>
2024-01-02 19:20:24 +0530

    updated exam2.cpp

--- Exam/exam2.cpp
+++ Exam/exam2.cpp
@@ -18,9 +18,38 @@ class stds : public abs{
         cout << "inside dervided class of abstract class" << endl;
     }
 };
+template <class A> class raja{
+    public:
+    A greatest(A x,A y,A z){
+        if(x>y && x>z){
+            return x; 
+        }
+        else if(y>x && y>z){
+            return y;
+        }
+        else{
+            return z;
+        }
+    }
+};
+
+template <class X> X exp(X a,X b){
+    if(a>b){
+        return a+b;
+    }
+    else{
+        return a-b;
+    }
+}
+
 int main(){
-    stds s1;
-    s1.f1();
-    s1.display();
+    // stds s1;
+    // s1.f1();
+    // s1.display();
+    // raja <double> r1;
+    // double x = r1.greatest(13.6,11,13.7);
+    // cout << "greatest number : " << x << endl;
+    long x=exp(3,5);
+    cout << x << endl;
     return 0;
 }
